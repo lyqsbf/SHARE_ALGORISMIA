62948db3f8d31a59025f5c9e5883ff2301a5affd
BacktrackMaster69 <alvaro.escalza@estudiantat.upc.edu>
2025-10-23 16:41:22 +0200

    Create PermutationsWithRepetition.cc

---
+++ bck_permutations/PermutationsWithRepetition.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void print(const int& n, const vector<int>& num, const vector<string>& words){
+    cout << '(';
+    bool primer = true;
+    for(int i = 0; i < n; ++i){
+        if(primer){
+            cout << words[num[i]];
+            primer = false;
+        }
+        else cout << ',' << words[num[i]];
+    }
+    cout << ')' << endl;
+}
+
+void f(int i,const int& n, vector<int>& num, const vector<string>& words){
+	if(i == n) return print(n, num, words);
+	
+	for(int j = 0; j < n; ++j ){
+        num[i] = j;
+        f(i + 1, n, num, words);
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+    vector<string> words(n);
+    for(string& s : words) cin >> s;
+	vector<int> num(n);
+	f(0, n, num, words);
+}
