b5119e7996b53bcbb0670bda617ce9ea455fb503
Claragr1 <claragr959@gmail.com>
2025-10-23 09:37:11 +0200

    Update Comptant_paraules_xules.cc

--- Comptant_paraules_xules.cc
+++ Comptant_paraules_xules.cc
@@ -5,15 +5,22 @@ using namespace std;
 
 using VI = vector<long long>;
 using VVI = vector<VI>;
-VVI memo;
+VVI memo = VVI(16, VI(16, -1));;
 int n, v;
 
 
-
 int f(int i, int voc, bool ant){
+    for (int j = 0; j < 16; ++j){
+        for (int k = 0; k < 16; ++k){
+            cout << ' ' << memo[j][k];
+        }
+        cout << endl;
+    }
+    cout << endl;
 	if (voc < 0) return 0;
 	if (i == 0) return (voc == 0) ? 1 : 0;
-	if (i > voc + 1) return 0;
+	if (i%2 == 0 and i/2 > voc) return 0;
+    if (i%2 == 1 and i/2 > voc) return 0;
 	
 	if (memo[i][voc] != -1) return memo[i][voc];
 	
@@ -30,7 +37,6 @@ int f(int i, int voc, bool ant){
 
 int main(){
     while (cin >> n >> v){
-		memo = VVI(16, VI(16, -1));
 		cout << f(n, v, false) << endl;
 	}
 }
