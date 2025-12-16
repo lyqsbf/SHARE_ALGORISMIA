545b08aa4f3a2f3cd3e0a256a0c12f1773d3a509
Didii12 <didac.ferrer.martinez@estudiantat.upc.edu>
2025-12-16 19:15:21 +0100

    Create P71636_UPC-3_Dídac.cc

---
+++ contest/2020/P71636_UPC-3_Dídac.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+int main() {
+	bool trobat = false;
+	int y = 10;
+	double x = 10;
+	while (not trobat) {
+		x = (11.0 / 9) * y;
+		int aux = x;
+
+		if (aux == x) trobat = true;
+		else ++y;
+	}
+	cout << x  << ' ' << y << endl;
+}*/
+
+int main() {
+	cout << 22 << endl << 18 << endl;
+}
